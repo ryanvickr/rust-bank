cc3b0c84edadf316df7aefaea1b058190f6c56c6
Ryan Vickramasinghe <ryanvickr@gmail.com>
2024-11-30 14:55:15 -0500

    Add duckDB import

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use duckdb::{params, Connection, Result};
+
 fn main() {
     println!("Hello, world!");
+
+    let _conn: Connection = Connection::open_in_memory().expect("Failed to open database.");
 }
