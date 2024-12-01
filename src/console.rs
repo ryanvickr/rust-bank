52c9f402e7c8d5748ee0be9e9daef666ef932c0a
Ryan Vickramasinghe <ryanvickr@gmail.com>
2024-12-01 16:56:07 -0500

    Added ability to fetch available accounts.

--- src/console.rs
+++ src/console.rs
@@ -1,7 +1,8 @@
 use duckdb::Error;
 use std::io::Write;
+use std::{fmt::Write as FmtWrite, num::ParseIntError};
 
-use crate::database::connection::{Database, User};
+use crate::database::connection::{Account, Database, User};
 
 const EXIT_CMD: &str = "exit";
 const HELP_CMD: &str = "help";
@@ -24,7 +25,11 @@ pub fn run_console(db: &Database) {
             EXIT_CMD => exit = true,
             HELP_CMD => print_help(),
             REGISTER_CMD => register(db),
-            _ => eprintln!("Unknown command: '{}'", trimmed_input),
+            ACCOUNTS_CMD => accounts(db),
+            _ => eprintln!(
+                "Unknown command: '{}'. Try 'help' for a full list.",
+                trimmed_input
+            ),
         }
     }
 }
@@ -37,6 +42,7 @@ fn print_help() {
 
 // Registers a new user. If the provided userID exists, nothing happens and an error is logged.
 fn register(db: &Database) {
+    // Get required registration information:
     let mut name: String = String::new();
     print!("Enter your name: ");
     std::io::stdout().flush().unwrap();
@@ -45,13 +51,7 @@ fn register(db: &Database) {
         .expect("Console error.");
     name = name.trim().to_string();
 
-    let mut user_id: String = String::new();
-    print!("Enter your username (no spaces/special chars): ");
-    std::io::stdout().flush().unwrap();
-    std::io::stdin()
-        .read_line(&mut user_id)
-        .expect("Console error.");
-    user_id = user_id.trim().to_string();
+    let user_id: String = get_user_id();
 
     let user: User = User {
         user_id,
@@ -72,3 +72,79 @@ fn register(db: &Database) {
         Err(error) => eprintln!("Failed to register new user: {}", error),
     };
 }
+
+fn accounts(db: &Database) {
+    // Fetch available accounts and list them:
+    let user_id: String = get_user_id();
+    let user: Result<User, Error> = db.get_user(user_id);
+    match user {
+        Err(error) => {
+            eprintln!("Failed to fetch a user: {}", error);
+            return;
+        }
+        Ok(user) => account_details(db, user),
+    }
+}
+
+fn account_details(db: &Database, user: User) {
+    println!(
+        "\nWelcome {}, you have {} accounts. Select an option:",
+        user.name,
+        user.accounts.len()
+    );
+    let mut options_msg: String = String::new();
+    let mut num_options: u8 = 0;
+    for account_id in &user.accounts {
+        writeln!(
+            &mut options_msg,
+            "\t{} - Account #{}",
+            num_options, account_id
+        )
+        .unwrap();
+        num_options += 1;
+    }
+    writeln!(&mut options_msg, "\t{} - Create new account", num_options).unwrap();
+    println!("{}", options_msg);
+
+    // Get the selected option
+    let mut selected_option_str: String = String::new();
+    print!("Select an option [0-{}]: ", num_options);
+    std::io::stdout().flush().unwrap();
+    std::io::stdin()
+        .read_line(&mut selected_option_str)
+        .expect("Console error.");
+    selected_option_str = selected_option_str.trim().to_string();
+    // Validate the selected option
+    let selected_option_res: Result<u8, ParseIntError> = selected_option_str.parse();
+    if selected_option_res.is_err() {
+        eprintln!("Not a valid number.");
+        return;
+    }
+    let selected_option: u8 = selected_option_res.unwrap();
+    if usize::from(selected_option) > user.accounts.len() {
+        eprintln!("Invalid selection.");
+        return;
+    }
+
+    // We have a valid selection. Check whether we are creating or retrieving an account.
+    if selected_option == num_options {
+        // Create an account
+
+    }
+}
+
+// Helper function gets the user ID input from the console.
+fn get_user_id() -> String {
+    let mut user_id: String = String::new();
+    print!("Enter your username (no spaces/special chars): ");
+    std::io::stdout().flush().unwrap();
+    std::io::stdin()
+        .read_line(&mut user_id)
+        .expect("Console error.");
+    user_id.trim().to_string()
+}
+
+// TODO: Finish this
+// fn create_account() -> Account {
+
+// }
--- src/database/connection.rs
+++ src/database/connection.rs
@@ -35,8 +35,23 @@ impl Database {
             params![user.user_id, user.name, user.accounts],
         )
     }
+
+    pub fn get_user(&self, user_id: String) -> Result<User, Error> {
+        self.conn.query_row(
+            "SELECT * FROM Users WHERE user_id = ?",
+            params![user_id],
+            |row| {
+                Ok(User {
+                    user_id: row.get(0)?,
+                    name: row.get(1)?,
+                    accounts: row.get(2)?,
+                })
+            },
+        )
+    }
 }
 
+#[derive(Debug)]
 pub struct User {
     pub user_id: String,
     pub name: String,
